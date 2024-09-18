c4055e29402d42fe71494dd6dc2a14c357d686ed
Rana <reduanulislam92665@gmail.com>
2024-09-18 22:52:08 +0600

    added evant haneler in progress

--- js/home.js
+++ js/home.js
@@ -1,1 +1,12 @@
-console.log('home added')
+// add money to the account
+
+// step 1: add event handeler to the  add money button inside the form
+document.getElementById('btn-add-primary').addEventListener('click', function(event){
+      event.preventDefault()
+//      step 2 : get money to be added to the account
+      const addMoneyInput = document.getElementById('input-add-money').value;
+      console.log(addMoneyInput)
+//     get the pin number provided 
+      const pinNUmber = document.getElementById('input-pin-number').value
+      console.log(pinNUmber)
+})

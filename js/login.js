3731dbcf1c792fb10fd21fd092d89fb4510bae3a
Rana <reduanulislam92665@gmail.com>
2024-09-18 21:45:25 +0600

    home page create

--- js/login.js
+++ js/login.js
@@ -1,10 +1,35 @@
 // console.log('Hello')
 // setp 1: event handler
-document.getElementById('btn-login').addEventListener('click', function(event){  
-      // setp 2: default behavior(prevent: reloading browser) 
-      event.preventDefault()   
-      console.log('login btn clicked')
-      // step 3: get the phone number
+// document.getElementById('btn-login').addEventListener('click', function(event){  
+//       // setp 2: default behavior(prevent: reloading browser) 
+//       event.preventDefault()   
+      
+//       // step 3: get the phone number and pin number
+//       const phoneNumber = document.getElementById('phone-number').value;
+//       const pinNumber = document.getElementById('pin-number').value
+//       console.log(phoneNumber, pinNumber);
+//       // step 4: validate phone and pin
+//       // this is not the ideal away
+//       if (phoneNumber === '5' && pinNumber === '1234') {
+//             console.log('you logged in')
+//             // step 5: allow user use the website
+//       }else{
+//             alert('wrong phone number or pin')
+//       }
+
+// })
+
+document.getElementById('btn-login').addEventListener('click', function(event){
+      event.preventDefault()
+      // get phoneNumber and pinNumber
       const phoneNumber = document.getElementById('phone-number').value
-      console.log(phoneNumber);
+      const pinNUmber = document.getElementById('pin-number').value
+      console.log(phoneNumber, pinNUmber)
+      // bad way to validate 
+      if (phoneNumber === '1234' && pinNUmber === '5') {
+            console.log('you logged in')
+            window.location.href = '/home.html'
+      }else{
+            alert('wrong phone number or pin')
+      }
 })
